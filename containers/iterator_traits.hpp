c1d94b8b89dd0935032da34152e057526bb68946
zhy2on <dhwlgus5940@gmail.com>
2022-09-04 15:18:21 +0900

    Fix: iterator_traits 불필요한 typename 선언 없앰

--- containers/iterator_traits.hpp
+++ containers/iterator_traits.hpp
@@ -6,15 +6,15 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:34:38 by jihoh             #+#    #+#             */
-/*   Updated: 2022/09/02 00:58:22 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/09/04 15:17:26 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_TRAITS_HPP
 #define ITERATOR_TRAITS_HPP
 
-#include <cstddef>
-#include <iterator>
+#include <cstddef> // ptrdiff_t
+#include <iterator> // random_access_tag
 
 namespace ft {
 template <class Iterator>
@@ -28,19 +28,19 @@ class iterator_traits {
 
 template <class T>
 class iterator_traits<T*> {
-    typedef typename ptrdiff_t difference_type;
-    typedef typename T value_type;
-    typedef typename T* pointer;
-    typedef typename T& reference;
+    typedef typename std::ptrdiff_t difference_type;
+    typedef T value_type;
+    typedef T* pointer;
+    typedef T& reference;
     typedef typename std::random_access_iterator_tag iterator_category;
 };
 
 template <class T>
 class iterator_traits<const T*> {
-    typedef typename ptrdiff_t difference_type;
-    typedef typename T value_type;
-    typedef const typename T* pointer;
-    typedef const typename T& reference;
+    typedef typename std::ptrdiff_t difference_type;
+    typedef T value_type;
+    typedef const T* pointer;
+    typedef const T& reference;
     typedef typename std::random_access_iterator_tag iterator_category;
 };
 } // namespace ft
