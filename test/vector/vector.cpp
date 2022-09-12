7b8653507b5290c97e28e55345d6f381d9ec39cf
zhy2on <dhwlgus5940@gmail.com>
2022-09-12 20:51:07 +0900

    Test: vector iterator test

--- test/vector/vector.cpp
+++ test/vector/vector.cpp
@@ -28,5 +28,15 @@ int main()
 	v4 = v2;
 	for (it = v4.begin(); it != v4.end(); it++)
 		std::cout << *it << " ";	
+	std::cout << std::endl;
+	
+	int k = 1;
+	std::cout << "iterator pointer test" << std::endl;
+	ft::vector_iterator<int>::pointer p = &k;
+	std::cout << *p << std::endl;
 
+	ft::reverse_iterator<ft::vector_iterator<int> > rit;
+	std::cout << "rit" << std::endl;
+	for (rit = v4.rbegin(); rit != v4.rend(); rit++)
+		std::cout << *rit << " ";
 }
