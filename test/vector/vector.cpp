b98e558da3e633bf4c16d55ad6549c1c921d6a26
zhy2on <dhwlgus5940@gmail.com>
2022-09-14 12:23:21 +0900

    Test: test file

--- test/vector/vector.cpp
+++ test/vector/vector.cpp
@@ -6,6 +6,7 @@ int main()
 	ft::vector<int> v(5, 2);
 	ft::vector<int>::iterator it;
 
+	std::cout << std::endl << "#====== begin end =====#" << std::endl;
 	for (it = v.begin(); it != v.end(); it++)
 		std::cout << *it << " ";
 
@@ -30,13 +31,29 @@ int main()
 		std::cout << *it << " ";	
 	std::cout << std::endl;
 	
+	std::cout << std::endl << "#====== iterator pointer =====#" << std::endl;
 	int k = 1;
-	std::cout << "iterator pointer test" << std::endl;
 	ft::vector_iterator<int>::pointer p = &k;
 	std::cout << *p << std::endl;
 
+	std::cout << std::endl << "#===== rit =====#" << std::endl;
 	ft::reverse_iterator<ft::vector_iterator<int> > rit;
-	std::cout << "rit" << std::endl;
 	for (rit = v4.rbegin(); rit != v4.rend(); rit++)
 		std::cout << *rit << " ";
+	std::cout << std::endl;
+
+	// std::cout << std::endl << "#===== front back =====#" << std::endl;
+	// ft::vector<int> vempty;
+	// std::cout << vempty.front() << std::endl;
+	// std::cout << vempty.back() << std::endl;
+
+	std::cout << std::endl << "#===== assign =====#" << std::endl;
+	v4.assign(v3.begin(), v3.end());
+	for (rit = v4.rbegin(); rit != v4.rend(); rit++)
+		std::cout << *rit << " ";
+	std::cout << std::endl;
+
+	std::cout << std::endl << "#===== push_back =====#" << std::endl;
+	
+
 }
