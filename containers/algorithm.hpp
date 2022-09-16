37b296199317980e3743275fb9dcf8e26339d59c
zhy2on <dhwlgus5940@gmail.com>
2022-09-16 15:49:58 +0900

    Feat: equal, lexicographical_compare 구현

--- containers/algorithm.hpp
+++ containers/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 20:38:33 by jihoh             #+#    #+#             */
-/*   Updated: 2022/09/15 20:51:38 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/09/16 15:42:55 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,59 @@ namespace ft
     template <class InputIterator1, class InputIterator2>
     bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
     {
-        while (first1 != last1)
+        for (; first1 != last1; ++first1, ++first2)
         {
-            if (!(*first1 == *first2)) // or: if (!pred(*first1,*first2)), for version 2
-                return (false);
-            ++first1;
-            ++first2;
+            if (!(*first1 == *first2))
+            {
+                return false;
+            }
         }
-        return (true);
+        return true;
+    }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+    bool equal(InputIterator1 first1, InputIterator1 last1,
+               InputIterator2 first2, BinaryPredicate pred)
+    {
+        for (; first1 != last1; ++first1, ++first2)
+        {
+            if (!pred(*first1, *first2))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     template <class InputIterator1, class InputIterator2>
     bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                                  InputIterator2 first2, InputIterator2 last2)
     {
-        while (first1 != last1)
+        for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2)
         {
-            if (first2 == last2 || *first2 < *first1)
-                return (false);
-            else if (*first1 < *first2)
-                return (true);
-            ++first1;
-            ++first2;
+            if (*first1 < *first2)
+                return true;
+            if (*first2 < *first1)
+                return false;
         }
-        return (first2 != last2);
+
+        return (first1 == last1) && (first2 != last2);
+    }
+
+    template <class InputIterator1, class InputIterator2, class Compare>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                                 InputIterator2 first2, InputIterator2 last2,
+                                 Compare comp)
+    {
+        for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2)
+        {
+            if (comp(*first1, *first2))
+                return true;
+            if (comp(*first2, *first1))
+                return false;
+        }
+
+        return (first1 == last1) && (first2 != last2);
     }
 
 } // namespace ft
