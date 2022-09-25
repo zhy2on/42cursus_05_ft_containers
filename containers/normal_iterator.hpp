f9d403c0c2ba9a66329705f06fecc6e6504d85d4
zhy2on <dhwlgus5940@gmail.com>
2022-09-25 16:13:09 +0900

    Fix: normal_iterator nom memberfunction 추가, 템플릿 함수로 구현해야하는 멤버변수들 수정

--- containers/normal_iterator.hpp
+++ containers/normal_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 17:49:33 by jihoh             #+#    #+#             */
-/*   Updated: 2022/09/22 23:41:57 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/09/25 15:59:50 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,14 @@ namespace ft
 		{
 		}
 
-		normal_iterator(pointer ptr) // initialization
-			: _current(ptr)
+		explicit normal_iterator(Iterator iter) // initialization
+			: _current(iter)
 		{
 		}
 
-		normal_iterator(const normal_iterator &rhs) // copy-constructor
-			: _current(rhs._current)
+		template<typename Iter>
+		normal_iterator(const normal_iterator<Iter> &rhs) // copy-constructor
+			: _current(rhs.base())
 		{
 		}
 
@@ -52,63 +53,71 @@ namespace ft
 		{
 		}
 
-		normal_iterator &operator=(const normal_iterator &rhs) // copy-assignment
+		Iterator base() const
+		{
+			return _current;
+		}
+
+		template<typename Iter>
+		normal_iterator &operator=(const normal_iterator<Iter> &rhs) // copy-assignment
 		{
 			if (this != &rhs)
 			{
-				_current = rhs._current;
+				_current = rhs.base();
 			}
-			return (*this);
+			return *this;
 		}
 
 		// a == b, a != b
-		bool operator==(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator==(const normal_iterator<Iter> &rhs) const
 		{
-			return (_current == rhs._current);
+			return (_current == rhs.base());
 		}
 
-		bool operator!=(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator!=(const normal_iterator<Iter> &rhs) const
 		{
-			return (_current != rhs._current);
+			return (_current != rhs.base());
 		}
 
 		// *a, a->
 		reference operator*() const
 		{
-			return (*_current);
+			return *_current;
 		}
 
 		pointer operator->() const
 		{
-			return (_current);
+			return _current;
 		}
 
 		// ++a, a++
 		normal_iterator &operator++()
 		{
 			++_current;
-			return (*this);
+			return *this;
 		}
 
 		normal_iterator operator++(int)
 		{
 			normal_iterator tmp(*this);
 			++(*this);
-			return (tmp);
+			return tmp;
 		}
 
 		// --a, a--
 		normal_iterator &operator--()
 		{
 			--_current;
-			return (*this);
+			return *this;
 		}
 
 		normal_iterator operator--(int)
 		{
 			normal_iterator tmp(*this);
 			--(*this);
-			return (tmp);
+			return tmp;
 		}
 
 		// a + n, a - n
@@ -123,29 +132,34 @@ namespace ft
 		}
 
 		// a - b
-		difference_type operator-(const normal_iterator &rhs) const
+		template<typename Iter>
+		difference_type operator-(const normal_iterator<Iter> &rhs) const
 		{
-			return (_current - rhs._current);
+			return (_current - rhs.base());
 		}
 
 		// a < b, a > b
-		bool operator<(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator<(const normal_iterator<Iter> &rhs) const
 		{
-			return (_current < rhs._current);
+			return (_current < rhs.base());
 		}
 
-		bool operator>(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator>(const normal_iterator<Iter> &rhs) const
 		{
-			return (_current > rhs._current);
+			return (_current > rhs.base());
 		}
 
 		// a <= b, a >= b
-		bool operator<=(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator<=(const normal_iterator<Iter> &rhs) const
 		{
 			return !(this > rhs);
 		}
 
-		bool operator>=(const normal_iterator &rhs) const
+		template<typename Iter>
+		bool operator>=(const normal_iterator<Iter> &rhs) const
 		{
 			return !(this < rhs);
 		}
@@ -154,20 +168,70 @@ namespace ft
 		normal_iterator &operator+=(const int &n)
 		{
 			_current += n;
-			return (*this);
+			return *this;
 		}
 
 		normal_iterator &operator-=(const int &n)
 		{
 			_current -= n;
-			return (*this);
+			return *this;
 		}
 
 		reference operator[](const int n) const
 		{
-			return (_current[n]);
+			return _current[n];
 		}
 	};
+
+	// Non-member functions
+	template <class Iterator1, class Iterator2>
+	bool operator==(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return (lhs.base() == rhs.base());
+	}
+
+	template <class Iterator1, class Iterator2>
+	bool operator!=(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template <class Iterator1, class Iterator2>
+	bool operator<(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return (lhs.base() < rhs.base());
+	}
+
+	template <class Iterator1, class Iterator2>
+	bool operator<=(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	template <class Iterator1, class Iterator2>
+	bool operator>(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return (rhs < lhs);
+	}
+
+	template <class Iterator1, class Iterator2>
+	bool operator>=(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return !(lhs < rhs);
+	}
+
+	template <typename Iterator>
+	normal_iterator<Iterator> operator+(
+		typename normal_iterator<Iterator>::difference_type n, const normal_iterator<Iterator> &it)
+	{
+		return normal_iterator<Iterator>(it.base() + n);
+	}
+
+	template <typename Iterator1, typename Iterator2>
+	typename normal_iterator<Iterator1>::difference_type operator-(const normal_iterator<Iterator1> &lhs, const normal_iterator<Iterator2> &rhs)
+	{
+		return lhs.base() - rhs.base();
+	}
 }
 
 #endif
