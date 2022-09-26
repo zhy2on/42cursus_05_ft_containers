bdd6a6210dad38d4a06cf842acc2fea7d0f0f8af
zhy2on <dhwlgus5940@gmail.com>
2022-09-27 06:08:12 +0900

    Test: map test

--- test/map/ft-map.cpp
+++ test/map/ft-map.cpp
@@ -6,9 +6,16 @@ int main()
 	ft::map<char, int> m;
 
 	m.insert(ft::make_pair('a', 1));
+	m.insert(ft::make_pair('b', 1));
+	m.insert(ft::make_pair('c', 1));
 
-	std::cout << m.find('a')->first << std::endl;
+	std::cout << m.begin()->first << std::endl;
 	m.erase(m.begin());
-	std::cout << m.find('a')->first << std::endl;
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
 	return (0);
 }
--- test/map/std-map.cpp
+++ test/map/std-map.cpp
@@ -4,10 +4,32 @@
 int main()
 {
 	std::map<char, int> m;
-	std::map<char, int>::iterator it;
+	std::map<int, int> m2;
 
 	m.insert(std::make_pair('a', 1));
+	m.insert(std::make_pair('b', 1));
+	m.insert(std::make_pair('c', 1));
 
-	std::cout << m.find('a')->first << std::endl;
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+	std::cout << m.begin()->first << std::endl;
+	m.erase(m.begin());
+
+	// m2.insert(std::make_pair(1, 1));
+	// m2.insert(std::make_pair(2, 1));
+	// m2.insert(std::make_pair(3, 1));
+
+	// std::cout << m2.begin()->first << std::endl;
+	// m2.erase(m2.begin());
+	// std::cout << m2.begin()->first << std::endl;
+	// m2.erase(m2.begin());
+	// std::cout << m2.begin()->first << std::endl;
+	// m2.erase(m2.begin());
+	// std::cout << m2.begin()->first << std::endl;
+	// m.erase(m.begin());
 	return (0);
 }
